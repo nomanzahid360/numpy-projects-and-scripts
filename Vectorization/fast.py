b95b28e059979f2e3f881a88f22262dc9903efe6
MUHAMMAD NOMAN ZAHID <nomanzahid360@gmail.com>
2025-10-24 17:11:38 +0500

    feat: Add Vectorization performance and problem module

---
+++ Vectorization/fast.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+
+result = arr1 + arr2
+print(result)  # Output: [5 7 9]
+
+# --- IGNORE ---
+
+arr3 =np.array([7, 8, 9])
+arr4 = np.array([10, 11, 12])
+result2 = arr3 * arr4
+print(result2)  # Output: [ 70  88 108]
---
+++ Vectorization/prob.py
@@ -0,0 +1,5 @@
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+
+result = [a + b for a, b in zip(list1, list2)]
+print(result)  # Output: [5, 7, 9]
